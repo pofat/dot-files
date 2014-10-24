c0698afdc14d14960e5e94a3dc7f5162e4fcf3ad
Mudox <mudoxisme@gmail.com>
2014-10-24 23:40:03 +0800

    mjolnir/init.lua: add 'open or raise' hotkeys.

--- mjolnir/init.lua
+++ mjolnir/init.lua
@@ -1,11 +1,9 @@
-local win   = require "mjolnir.window"
-local app   = require "mjolnir.application"
-local key   = require "mjolnir.hotkey"
-local win   = require "mjolnir.window"
-local win   = require "mjolnir.window"
-local futl  = require "mjolnir.fnutils"
-local alert = require "mjolnir.alert"
-local grid  = require "mjolnir.bg.grid"
+local application = require "mjolnir.application"
+local hotkey      = require "mjolnir.hotkey"
+local window      = require "mjolnir.window"
+local fnutils     = require "mjolnir.fnutils"
+local alert       = require "mjolnir.alert"
+local grid        = require "mjolnir.bg.grid"
 
 
 -- Set up hotkey combinations
@@ -18,35 +16,46 @@ grid.GRIDHEIGHT = 3
 grid.MARGINX    = 0
 grid.MARGINY    = 0
 
-key.bind(a_s, "R", function() mjolnir.reload() end)
+hotkey.bind(a_s, "R", function() mjolnir.reload() end)
 
-key.bind(a_s, "L", function() win.focusedwindow():focuswindow_east() end)
-key.bind(a_s, "H", function() win.focusedwindow():focuswindow_west() end)
-key.bind(a_s, "K", function() win.focusedwindow():focuswindow_north() end)
-key.bind(a_s, "J", function() win.focusedwindow():focuswindow_south() end)
+-- jump between windows.
+hotkey.bind(a_s, "L", function() window.focusedwindow():focuswindow_east() end)
+hotkey.bind(a_s, "H", function() window.focusedwindow():focuswindow_west() end)
+hotkey.bind(a_s, "K", function() window.focusedwindow():focuswindow_north() end)
+hotkey.bind(a_s, "J", function() window.focusedwindow():focuswindow_south() end)
 
-key.bind(a_s, ';', function() grid.snap(win.focusedwindow()) end)
-key.bind(a_s, "'", function() futl.map(win.visiblewindows(), grid.snap) end)
-key.bind(a_s, '=', function() grid.adjustwidth( 1) end)
-key.bind(a_s, '-', function() grid.adjustwidth(-1) end)
+-- grid manipulation.
+hotkey.bind(a_s, ';', function() grid.snap(window.focusedwindow()) end)
+hotkey.bind(a_s, "'", function() fnutils.map(window.visiblewindows(), grid.snap) end)
+hotkey.bind(a_s, '=', function() grid.adjustwidth( 1) end)
+hotkey.bind(a_s, '-', function() grid.adjustwidth(-1) end)
 
-key.bind(a_s, 'M', grid.maximize_window)
 
-key.bind(a_s, 'N', grid.pushwindow_nextscreen)
-key.bind(a_s, 'P', grid.pushwindow_prevscreen)
+-- transfer windows between screens.
+hotkey.bind(a_s, 'N', grid.pushwindow_nextscreen)
+hotkey.bind(a_s, 'P', grid.pushwindow_prevscreen)
 
-key.bind(c_s, 'J', grid.pushwindow_down)
-key.bind(c_s, 'K', grid.pushwindow_up)
-key.bind(c_s, 'H', grid.pushwindow_left)
-key.bind(c_s, 'L', grid.pushwindow_right)
+-- move windows.
+hotkey.bind(c_s, 'J', grid.pushwindow_down)
+hotkey.bind(c_s, 'K', grid.pushwindow_up)
+hotkey.bind(c_s, 'H', grid.pushwindow_left)
+hotkey.bind(c_s, 'L', grid.pushwindow_right)
 
-key.bind(a_s, 'D', grid.pushwindow_down)
-key.bind(a_s, 'E', grid.pushwindow_up)
-key.bind(a_s, 'S', grid.pushwindow_left)
-key.bind(a_s, 'F', grid.pushwindow_right)
+hotkey.bind(a_s, 'D', grid.pushwindow_down)
+hotkey.bind(a_s, 'E', grid.pushwindow_up)
+hotkey.bind(a_s, 'S', grid.pushwindow_left)
+hotkey.bind(a_s, 'F', grid.pushwindow_right)
 
-key.bind(a_s, 'U', grid.resizewindow_taller)
-key.bind(a_s, 'O', grid.resizewindow_wider)
+-- resize windows.
+hotkey.bind(a_s, 'Y', grid.resizewindow_shorter)
+hotkey.bind(a_s, 'U', grid.resizewindow_taller)
 
-key.bind(a_s, 'I', grid.resizewindow_thinner)
-key.bind(a_s, 'Y', grid.resizewindow_shorter)
+hotkey.bind(a_s, 'O', grid.resizewindow_wider)
+hotkey.bind(a_s, 'I', grid.resizewindow_thinner)
+
+hotkey.bind(a_s, 'M', grid.maximize_window)
+
+-- open or raise applications.
+hotkey.bind(c_s, 'F', function() application.launchorfocus("firefox") end)
+hotkey.bind(c_s, 'I', function() application.launchorfocus("iterm") end)
+hotkey.bind(c_s, 'V', function() application.launchorfocus("macvim") end)
