d3592716bb37bcdf284482afb47ec162a0549be8
mudox <mudoxisme@gmail.com>
2015-10-15 10:40:32 +0800

    scripts/compile_ycm.sh: switch to use install.py

--- scripts/cmdhub/osx/compile_ycm.sh
+++ scripts/cmdhub/osx/compile_ycm.sh
@@ -1,2 +1,3 @@
 cd "${MDX_REPOS_ROOT}/vim-config/plugged/ycm" && \
-PATH="/usr/bin:$PATH" ./install.sh --system-libclang --clang-completer --gocode-completer
+#PATH="/usr/bin:$PATH" ./install.py --clang-completer --gocode-completer
+./install.py --clang-completer --gocode-completer
