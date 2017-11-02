1c169bc6aadbd72eabd44a9f5eb3380443de9def
mudox <mudoxisme@gmail.com>
2017-11-02 20:02:34 +0800

    mdx-choose-tree: fix fzf field showing bug

--- tmux/mdx-choose-tree.sh
+++ tmux/mdx-choose-tree.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-target=$(tmux list-windows -a -F '#{window_id}	#S:#W' | fzf-tmux -d30% --with-nth=2 | cut -f1)
+_pick_a_window() {
+fzf-tmux -d30% \
+  --with-nth=2..
+}
+
+target=$(tmux list-windows -a -F '#{window_id}	#S:#W' | _pick_a_window | cut -f1)
 [[ -z $target ]] && return
 
 tmux switch-client -t "$target" &>/dev/null
