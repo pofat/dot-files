a1786b2e7eac0a66b4f9bc6612b2c6be741130b0
mudox <mudoxisme@gmail.com>
2017-11-04 03:12:41 +0800

    install.sh: add jack loggings

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# vim: fdm=marker
+
+source ~/Git/dot-files/shell/jack_log
 
 DOT=~/Git/dot-files
 VIM=~/Git/vim-config
@@ -7,12 +10,16 @@ VIM=~/Git/vim-config
 
 mkdir ~/.config &>/dev/null
 
+jackNote '\nINSTALL DOT FILES\n'
+
 # neovim
+echo '- Neovim'
 ln -sf -T $VIM ~/.vim
 ln -sf -T $VIM ~/.config/nvim
 ln -sf -T $VIM/ctrlp-cmdhub-jobs ~/.ctrlp-cmdhub-jobs
 
 # tmux
+echo '- Tmux'
 ln -sf $DOT/tmux/tmux.conf ~/.tmux.conf
 
 # git
@@ -20,19 +27,24 @@ ln -sf $DOT/git/gitconfig ~/.gitconfig
 ln -sf $DOT/git/cvsignore ~/.cvsignore
 
 # zsh
+echo '- Zsh'
 ln -sf $DOT/zshrc ~/.zshrc
 
 # hammerspoon
+echo '- Hammerspoon'
 ln -sf -T $DOT/hammerspoon ~/.hammerspoon
 
-# karabiner
+# karabiner-element
+echo '- Karabiner-Element'
 ln -sf -T $DOT/karabiner ~/.config/karabiner
 
 # python
+echo '- Python'
 ln -sf -T $DOT/python/yapf ~/.config/yapf
 ln -sf -T $DOT/python/pip ~/Library/Application\ Support/pip
 
-# firefox
+# vimperator
+echo '- Vimperator'
 ln -sf $DOT/vimperatorrc ~/.vimperatorrc
 # }}}# vim: fdm=marker
 
