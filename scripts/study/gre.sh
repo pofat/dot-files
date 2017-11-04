a59483633340f4b61717ca896dc9220cfd4e341a
mudox <mudoxisme@gmail.com>
2017-11-05 07:15:57 +0800

    Remove `scripts` folder

--- scripts/study/gre.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-gvim +cd '~/Dropbox/TEXT.DROP/vocabulary/ymh gre/' \
-    +e gre_ymh.txt +vnew confusion.txt \
-    +new tough_words.txt
--- scripts/study/lyn.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-gvim -S ~/vim/lyn5500 & vlc & exit
