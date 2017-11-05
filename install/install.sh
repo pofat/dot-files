b6dcef4d05615bb79cadf14d953ee010c6e82162
mudox <mudoxisme@gmail.com>
2017-11-05 21:34:39 +0800

    instal.sh: add `lldb`, `ssh` install steps, improve echoing

--- install/install.sh
+++ install/install.sh
@@ -15,44 +15,122 @@ mkdir ~/.config &>/dev/null
 jackNote '\nInstall Dot Files\n'
 
 # neovim
-echo '- Neovim'
+cat <<END
+
+- Neovim
+  ~/.vim
+  ~/.config/nvim
+  ~/.ctrlp-cmdhub-jobs
+
+END
+
 ln -sf -T $VIM ~/.vim
 ln -sf -T $VIM ~/.config/nvim
 ln -sf -T $VIM/ctrlp-cmdhub-jobs ~/.ctrlp-cmdhub-jobs
 
+# ssh
+cat <<END
+- SSH
+  ~/.ssh
+
+END
+
+ln -sf -T $DOT/ssh ~/.ssh
+
+# LLDB
+cat <<END
+- LLDB
+  ~/.lldb
+
+END
+
+ln -sf $DOT/lldbinit ~/.lldbinit
+
 # tmux
-echo '- Tmux'
+cat <<END
+- Tmux
+  ~/.tmux.conf
+
+END
+
 ln -sf $DOT/tmux/tmux.conf ~/.tmux.conf
 
 # git
+cat <<END
+- Git
+  ~/.gitconfig
+  ~/.cvsignore
+
+END
+
 ln -sf $DOT/git/gitconfig ~/.gitconfig
 ln -sf $DOT/git/cvsignore ~/.cvsignore
 
 # zsh
-echo '- Zsh'
+cat <<END
+- Zsh
+  ~/.zshrc
+
+END
+
 ln -sf $DOT/zshrc ~/.zshrc
 
 # hammerspoon
-echo '- Hammerspoon'
+cat <<END
+- Hammerspoon
+  ~/.hammerspoon
+
+END
 ln -sf -T $DOT/hammerspoon ~/.hammerspoon
 
 # karabiner-element
-echo '- Karabiner-Element'
+cat <<END
+- Karabiner-Element
+  ~/.config/karabiner
+
+END
+
 ln -sf -T $DOT/karabiner ~/.config/karabiner
 
 # python
-echo '- Python'
+cat <<END
+- Python
+  ~/.config/yapf
+  ~/Library/Application Support/pip
+
+END
 ln -sf -T $DOT/python/yapf ~/.config/yapf
 ln -sf -T $DOT/python/pip ~/Library/Application\ Support/pip
 
 # vimperator
-echo '- Vimperator'
+cat <<END
+- Vimperator
+  ~/.vimperatorrc
+
+END
+
 ln -sf $DOT/vimperatorrc ~/.vimperatorrc
-# }}}# vim: fdm=marker
 
 # fzf-cmdhub
-echo '- Utility: fzf-cmdhub'
+cat <<END
+- Utility: fzf-cmdhub
+  ~/.config/fzf-cmdhub
+for dir in "$HOME" "$HOME/.config" "$HOME/Library/Application Support"; do
+END
 ln -sf -T $DOT/fzf-cmdhub ~/.config/fzf-cmdhub
+
+# Show results
+jackInfo '\nMade following symbolic links:\n'
+for dir in "$HOME" "$HOME/.config"; do
+  echo
+  find "$dir" -type l -maxdepth 1 | while read link; do
+    printf '+ %-26s   ->   %s\n'                   \
+      $(realpath --relative-base=$HOME -s "$link")   \
+      $(realpath --relative-base=$HOME -e "$link")
+  done
+done
+
+# }}}
 
 # HOMEBREW {{{
 
@@ -66,6 +144,7 @@ else
 
   jackNote '\nInstall Formulas\n'
   brew install $(<$SDIR/brew-list)
+  brew doctor
 fi
 
 # }}}
