7aac59862c4f1f2d3d91f7ee35e66b7c4f631d5e
mudox <mudoxisme@gmail.com>
2017-11-07 01:50:04 +0800

    tmux: add symbols to `fzf-choose-tree`

--- tmux/fzf-choose-tree.sh
+++ tmux/fzf-choose-tree.sh
@@ -7,13 +7,20 @@ _fzf() {
     --margin=1
 }
 
+declare -A _symbols
+_symbols[Configure]=
+_symbols[Update]=
+_symbols[Dashboard]=
+_symbols[Play]=
+
 _target_lines() {
   session_lines=$(tmux list-sessions -F '#{session_id} #S')
   while read session_id session_name; do
-    printf '\n%s\n' "$session_id        [38;5;16m $session_name[0m"
+    symbol="${_symbols[$session_name]:-}"
+    printf '\n%s\n' "$session_id        $symbol [38;5;16m$session_name[0m"
     window_lines=$(tmux list-windows -t "${session_id}" -F '#{window_id} #W')
     while read window_id window_name; do
-      printf "%s\t - %-38s%20s\n" "$window_id" "[38;5;048m$window_name[0m" "[38;5;242m$session_name[0m"
+      printf "%s\t -  %-32s%-20s\n" "$window_id" "[38;5;048m$window_name[0m" "[38;5;242m$session_name[0m"
     done <<< "$window_lines"
   done <<< "$session_lines"
 }
