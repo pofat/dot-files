37de060dc10a455460e6eeaf87ac16662557fa13
mudox <mudoxisme@gmail.com>
2017-11-13 23:21:12 +0800

    tmux/choose-tree.sh: session line can jump again

--- tmux/fzf-choose-tree.sh
+++ tmux/fzf-choose-tree.sh
@@ -4,20 +4,21 @@ _fzf() {
   fzf --with-nth=2.. \
     --tiebreak=index \
     --ansi           \
-    --margin=1
+    --height=100%    \
+    --margin=0
 }
 
-declare -A _symbols
-_symbols[Configure]=
-_symbols[Update]=
-_symbols[Dashboard]=
-_symbols[Play]=
-
 _target_lines() {
+  declare -A _symbols
+  _symbols[Configure]=
+  _symbols[Update]=
+  _symbols[Dashboard]=
+  _symbols[Play]=
+
   session_lines=$(tmux list-sessions -F '#{session_id} #S')
   while read session_id session_name; do
     symbol="${_symbols[$session_name]:-}"
-    printf '\n%s\n' "$session_id        $symbol [38;5;16m$session_name[0m"
+    printf "\n%s\t %s \n" "$session_id" "$symbol [38;5;16m$session_name[0m"
     window_lines=$(tmux list-windows -t "${session_id}" -F '#{window_id} #W')
     while read window_id window_name; do
       printf "%s\t -  %-32s%-20s\n" "$window_id" "[38;5;048m$window_name[0m" "[38;5;242m$session_name[0m"
@@ -25,5 +26,15 @@ _target_lines() {
   done <<< "$session_lines"
 }
 
-target=$(_target_lines | _fzf | cut -f1)
-tmux switch-client -t "$target" &>/dev/null
+_update() {
+  local dst="$HOME/.local/share/tmux-choose-tree/tree" &>/dev/null
+  mkdir -p "$(dirname "${dst}")"
+  _target_lines > "${dst}"
+}
+
+_choose_tree() {
+  target=$(_target_lines | _fzf | cut -f1)
+  tmux switch-client -t "$target" &>/dev/null
+}
+
+_choose_tree
