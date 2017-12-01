0363d1281e274c6549e3465d18c118d9e89da80d
mudox <imudox@gmail.com>
2017-12-02 07:05:46 +0800

    add bash/new_repo.sh

---
+++ bash/new_repo.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+source ~/Git/dot-files/bash/lib/jack
+
+usage='$0 repo-name'
+
+if [[ ! $# -eq 1 ]]; then
+  jackError "$usage"
+  exit 1
+fi
+
+jackNote 'Creating git repo'
+git init
+git add --all
+git commit -m 'Initial commit'
+
+jackInfo "Creating github.com/mudox/$1"
+hub create "$1"
+
+jackInfo 'Adding .gitignore'
+types=$(git ignore list | tr ',' '\n' | fzf -m)
+for type in ${types}; do
+  git ignore "${type}"
+done
+
+jackNote 'Performing the initial `git push`'
+git push --set-upstream origin master
+
+hub browse
