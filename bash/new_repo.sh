ae26322750cc91c1d3c7a44cad1bb5bd102dd47d
mudox <imudox@gmail.com>
2017-12-03 00:48:43 +0800

    bash/new_repo.sh: improve logging

--- bash/new_repo.sh
+++ bash/new_repo.sh
@@ -10,21 +10,32 @@ if [[ ! $# -eq 1 ]]; then
   exit 1
 fi
 
-jackInfo 'Creating git repo'
+jackInfo '\nCreating git repo'
+sleep 1
+
 git init
 git add --all
 git commit -m 'Initial commit'
 
-jackInfo "Creating github.com/mudox/$1"
+jackInfo "\nCreating github.com/mudox/$1"
+sleep 1
+
 hub create "$1"
 
-jackInfo 'Adding .gitignore'
+jackInfo '\nCreating .gitignore'
+sleep 1
+
 types=$(git ignore list | tr ',' '\n' | fzf -m)
 for type in ${types}; do
   git ignore "${type}" >> .gitignore
 done
 
-jackInfo 'Performing the initial `git push`'
+jackInfo '\nPerforming initial push'
+sleep 1
+
 git push --set-upstream origin master
+
+jackInfo "\nDone, bringing you to github.com/mudox/$1"
+sleep 1
 
 hub browse
